f5de102abc6bf339e9d2d7982150a5511bd1c952
zhuxingsheng <zhuxingsheng@gmail.com>
2025-05-29 18:04:02 +0800

    541. 反转字符串II

---
+++ src/leecode/541.ts
@@ -0,0 +1,44 @@
+/**
+ * 541. 反转字符串II
+力扣题目链接(opens new window)
+
+给定一个字符串 s 和一个整数 k，从字符串开头算起, 每计数至 2k 个字符，就反转这 2k 个字符中的前 k 个字符。
+
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+
+示例:
+
+输入: s = "abcdefg", k = 2
+输出: "bacdfeg"
+
+
+344道题的加强版本
+
+*/
+function resverChar2(s:string,k:number):string{
+
+    let arr:string[] = s.split('');
+
+    for(let i=0;i<arr.length;i+=2*k){
+        let si = i;
+        let se = i + k-1;
+        if(arr.length - i < k ) {
+            se = arr.length-1
+        }
+
+        while(si<se) {
+            let s = arr[si];
+            arr[si] = arr[se]
+            arr[se] = s
+            si++
+            se--
+        }
+    }
+    return arr.join('')
+
+}
+
+console.log(resverChar2("abcdefghij",3))
+
